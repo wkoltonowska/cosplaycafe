12814e06677cbfbbbcfa19dfce8407d74ab993cb
Weronika Kołtonowska <szumigalskaweronika.gmail.com>
2024-05-25 22:25:39 +0200

    menu reservation corrected

--- js/rezerwacje.js
+++ js/rezerwacje.js
@@ -1,4 +1,30 @@
+const navContainer = document.querySelector(".nav__container");
+const navBars = document.querySelector(".nav__bars");
+const navItems = document.querySelectorAll(".js-items");
+const navExit = document.querySelector(".nav__exit");
 const footerYear = document.querySelector(".footer__year");
+let isShowedNav = false;
+
+navItems.forEach((item) => {
+	item.addEventListener("click", (event) => {
+		if (isShowedNav) {
+			navContainer.classList.remove("nav__container--active");
+			navExit.classList.remove("nav__exit--active");
+		}
+	});
+});
+
+const showNav = () => {
+	isShowedNav = true;
+	navContainer.classList.add("nav__container--active");
+	navExit.classList.add("nav__exit--active");
+};
+
+const hideNav = () => {
+	isShowedNav = false;
+	navContainer.classList.remove("nav__container--active");
+	navExit.classList.remove("nav__exit--active");
+};
 
 const currentYear = () => {
 	const year = new Date().getFullYear();
@@ -6,14 +32,6 @@ const currentYear = () => {
 };
 
 currentYear();
-const scrollToSection = (selector) => {
-	const navigationHeight = document.querySelector(".nav").offsetHeight + 30;
-	const targetDiv = document.querySelector(`${selector} .section__heading`);
-	const targetDivPosition = targetDiv.offsetTop - navigationHeight;
 
-	window.scrollTo({
-		top: targetDivPosition,
-	});
-
-	console.log(targetDivPosition);
-};
+navBars.addEventListener("click", showNav);
+navExit.addEventListener("click", hideNav);
